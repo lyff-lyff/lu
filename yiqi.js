76945085957653cc5a28f191a7c49bd1d33dc25d
lyff-lyff <83768457+lyff-lyff@users.noreply.github.com>
2021-08-09 14:56:53 +0800

    Update yiqi.js

--- yiqi.js
+++ yiqi.js
@@ -6,9 +6,9 @@ window.onload=function(){
 	buhao.onclick=function(){
 		Dianji++;
 		if(Dianji==1){
-	   			alert("小姐姐在考虑一下呗");
+	   			alert("小姐姐再考虑一下呗");
 	   }else if(Dianji==2){
-		   		alert("你是我见过的最帅气善良可爱的男孩");
+		   		alert("我是你见过的最帅气善良可爱的男孩");
 		   		
 	   }else if(Dianji==3){
 		   		alert("一生一世爱你");
@@ -19,9 +19,9 @@ window.onload=function(){
 	  }else if(Dianji==5){
 		   		alert("不藏私房钱");
 	  }else if(Dianji==6){
-		   		alert("房子写你名");
+		   		alert("我可是有房的人了");
 	  }else if(Dianji==7){
-		   		alert("工资全上交");
+		   		alert("你信不信我让你关不掉");
 		  		Dianji=1;
 	  }
 
